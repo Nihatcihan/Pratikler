65cfda25935063b0a1e85d1bb325800a8b51d644
Nihat Cihan <nihat.cihan7@gmail.com>
2024-10-31 15:26:08 +0300

    if else pratik

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int time = DateTime.Now.Hour;
+if (time>=6 && time <11)
+    Console.WriteLine("Gunaydin");
+else if (time <= 18)
+    Console.WriteLine("Iyi gunler");
+else 
+    Console.WriteLine("Iyi geceler");
+
+    string sonuc = time <=18 ? "Iyi gunler" : "Iyi geceler";
+        Console.WriteLine(sonuc);
+
+    sonuc = time >= 6 && time < 11 ? "Gunaydin" : time <= 18 ? "Iyi gunler" : "Iyi Geceler";
+        
+        Console.WriteLine(sonuc);
+
