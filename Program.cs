e336a7040a81a180316b2cf062b4f6e743b117d1
Nihat Cihan <nihat.cihan7@gmail.com>
2024-11-01 19:02:04 +0300

    while ve foreach calisildi

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// while
+// 1 den başlayarak consoledan girilen sayıya kadar sayı dahil ortalama hesaplayıo konsola yazdır
+// Console.Write("Lutfen bir sayi giriniz:");
+// int sayi= int.Parse(Console.ReadLine());
+// int sayac=1;
+// int toplam=0;
+// while (sayac<=sayi)
+// {
+//     toplam+= sayac;
+//     ++sayac;
+
+// }
+// Console.WriteLine(toplam/sayi);
+
+// // a'dan z'ye kadar tüm harfleri consola yazdir
+
+// char character= 'a';
+// while (character<'z')
+// {
+//     Console.Write(character);
+//     ++character;
+// }
+Console.WriteLine("*** For Each****");
+
+string[] arabalar = {"BMW","Ford","Toyota","Nissan","Ferrari"};
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## while ve foreach calisildi
