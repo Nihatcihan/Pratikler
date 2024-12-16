900188a30cb3d219fa36873648e75f567da0f26e
Nihat Cihan <nihat.cihan7@gmail.com>
2024-12-16 14:06:26 +0300

    enum

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(((int)Gunler.Pazar)+1);
+        int sicaklik=32;
+
+        if (sicaklik <= (int)HavaDurumu.Normal)
+            Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını beklelim");
+
+        else if(sicaklik>= (int)HavaDurumu.Sıcak)
+            Console.WriteLine("Dışarıya çıkmak çok sıcak bir gün");
+
+        else if(sicaklik>= (int)HavaDurumu.Normal && sicaklik<= (int)HavaDurumu.CokSıcak)
+            Console.WriteLine("Hadi dışarıya çıkalım");
+    }   
+}
+enum Gunler
+{
+    Pazartesi,
+    Sali,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+
+}
+enum HavaDurumu
+{
+    Soguk=5,
+    Normal=20,
+    Sıcak=25,
+    CokSıcak=30
+}
